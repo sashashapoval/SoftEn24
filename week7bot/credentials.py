efe142e524b240e63891455d28d258298fd6aaea
Sasha Shapoval <60059635+sashashapoval@users.noreply.github.com>
2024-04-23 14:29:58 +0200

    Delete credentials.py

--- week7bot/credentials.py
+++
@@ -1,4 +0,0 @@
-bot_token = "1930966075:AAHS7UujisibF1aEX6f_81hba_kQq_8kIRc"
-bot_user_name = "smartMo_bot"
-URL = "the heroku app link that we will create later"
-#https://habr.com/ru/sandbox/149884/
